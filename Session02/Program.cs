260b0f3ffab66fd4dbaaaa492bd24a02f74b8483
saridonus <32794524+saridonus@users.noreply.github.com>
2023-03-16 16:32:56 +0200

    New

---
+++ Session02/Program.cs
@@ -0,0 +1,13 @@
+﻿for (int i=0;i<10; i++)
+{
+    Console.WriteLine(i);
+}
+
+
+List<int> numbers = new List<int> { 1,2,3 };
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);
+    
+    
+}
